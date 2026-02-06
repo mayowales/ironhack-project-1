96bb266fd4832a13e5bee24ba187789434aa651a
mayowales <walekolade45@gmail.com>
2026-02-06 16:52:23 +0100

    update main module files

---
+++ terraform/main.tf
@@ -0,0 +1,22 @@
+# Call the Network Module
+module "network" {
+  source               = "./modules/networking"
+  vpc_cidr             = var.vpc_cidr
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+}
+
+# Call the Compute Module
+module "compute" {
+  source            = "./modules/compute"
+  vpc_id            = module.network.vpc_id
+  vpc_cidr          = module.network.vpc_cidr_block
+  # We take the first ID from the list outputs
+  public_subnet_id  = module.network.public_subnet_ids[0]
+  private_subnet_id = module.network.private_subnet_ids[0]
+  
+  ami_id            = var.ami_id
+  instance_type     = var.instance_type
+  key_name          = var.key_name
+  instance_map      = var.instance_map
+}
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -4,7 +4,16 @@ private_subnet_cidrs = ["10.0.2.0/24"]
 
 
 instance_map = {
-  "vote-app" = { is_public = true }
+  "frontend-app" = { is_public = true }
   "worker"   = { is_public = false }
-  "db"       = { is_public = false }
+  "db-postgres"       = { is_public = false }
 }
+
+aws_region           = "eu-central-1"
+vpc_cidr             = "10.0.0.0/16"
+public_subnet_cidrs  = ["10.0.1.0/24"]
+private_subnet_cidrs = ["10.0.2.0/24"]
+
+ami_id               = "ami-0a854fe96e0b45e4e"
+instance_type        = "t2.micro"
+key_name             = "wale-reg-b-key" 
